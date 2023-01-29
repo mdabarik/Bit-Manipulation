4b6b99f7b693a1d5d04b9fe2e759f58d75c63cbf
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-01-29 17:00:41 +0600

    Create 08--counting-bits.java

---
+++ 08--counting-bits.java
@@ -0,0 +1,36 @@
+/* 8# Counting Bits
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+    
+Example 1:
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+    
+Example 2:
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101 */
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        int offset = 1;
+        for (int i = 1; i <= n; i++) {
+            if (offset*2 == i) {
+                offset = i;
+            }
+            dp[i] = 1 + dp[i - offset];
+        }
+        return dp;
+    }
+}
